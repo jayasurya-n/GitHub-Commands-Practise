a216f8bff814b55d1691e475ae61a63db73b2d66
Jayasurya <jasu16902@gmail.com>
2024-04-29 15:04:30 +0530

    New changes again

--- index1.js
+++ index1.js
@@ -1,15 +1,3 @@
 import express from "express";
-import superheroes from "superheroes";
+import superheroes from "";
 
-const app = express();
-const port = 3000;
-
-
-app.get("/", (req, res) => {
-  let name = superheroes.random();
-  res.send(`<h1>Behold, I am ${name}!</h1>`);
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
